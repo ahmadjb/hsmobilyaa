c01b98960d3721ac08b2c6b2f6545730bcf1d57e
ahmad cbeılı <ahmad119966.aa@gmial.com>
2023-12-28 21:28:12 +0300

    revised main photos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,114 +10,107 @@ import photo1 from '../../Data/1st.webp';
 import photo2 from '../../Data/2nd.webp';
 import photo3 from '../../Data/3rd.webp';
 import photo4 from '../../Data/4th.webp';
+import { Height } from "@mui/icons-material";
 
 function Home(props) {
-    const nonThemeColor = useSelector(state => state.nonThemeColor);
-    const uiColor = useSelector(state => state.uiColor);
+  const nonThemeColor = useSelector(state => state.nonThemeColor);
+  const uiColor = useSelector(state => state.uiColor);
+  const [isMobile, setIsMobile] = useState(window.innerWidth);
+  const [mainPhoto, setMainPhoto] = useState(isMobile < 800 ? '20vh' : '55vh');
 
-    function handleTyper() {
-        let textItems = autoTypeData;
-        var autoTyper = document.getElementById('typer');
-        new Typewriter(autoTyper, {
-            strings: textItems,
-            autoStart: true,
-            pauseFor: 1000,
-            loop: true,
-        });
-    }
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      setIsMobile(newWidth);
+      setMainPhoto(newWidth < 800 ? '20vh' : '55vh');
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array to run the effect only once on mount
+
+  function handleTyper() {
+    let textItems = autoTypeData;
+    var autoTyper = document.getElementById('typer');
+    new Typewriter(autoTyper, {
+      strings: textItems,
+      autoStart: true,
+      pauseFor: 1000,
+      loop: true,
+    });
+  }
+
+  useEffect(() => {
+    handleTyper();
+  }, []);
+
+  const PhotoCarousel = () => {
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+    const photos = [photo1, photo2, photo3, photo4];
 
     useEffect(() => {
-        handleTyper();
-    }, []);
+      // Set up an interval to automatically navigate every 3 seconds
+      const intervalId = setInterval(() => {
+        setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
+      }, 3000);
 
-    const PhotoCarousel = () => {
-        const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
-        const photos = [photo1, photo2, photo3, photo4];
+      // Clear the interval when the component is unmounted
+      return () => clearInterval(intervalId);
+    }, [photos]);
 
-        useEffect(() => {
-            // Set up an interval to automatically navigate every 3 seconds
-            const intervalId = setInterval(() => {
-                setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
-            }, 3000);
+    const navigateToNextPhoto = () => {
+      setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
+    };
 
-            // Clear the interval when the component is unmounted
-            return () => clearInterval(intervalId);
-        }, [photos]);
+    const navigateToPrevPhoto = () => {
+      setCurrentPhotoIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
+    };
 
-        const navigateToNextPhoto = () => {
-            setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
-        };
-
-        const navigateToPrevPhoto = () => {
-            setCurrentPhotoIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
-        };
-
-        const transitionStyle = {
-            transition: 'left 0.5s ease-in-out',
-            left: `-${currentPhotoIndex * 100}%`, // Adjust based on the width of your images
-            position: 'relative',
-            width: '100%',
-            display: 'flex',
-        };
-
-        const arrowContainerStyleLeft = {
-            position: 'relative',
-            left: '2%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            zIndex: 1,
-        };
-        const arrowContainerStyleRight = {
-            position: 'absolute',
-            top: '45%',
-            left: '98%',
-            
-            display: 'flex',
-            justifyContent: 'space-between',
-            zIndex: 1,
-        };
-
-        return (
-            <div>
-                <div style={{ borderColor: 'red', borderRadius: 10, borderStyle: 'solid', borderWidth: 2 }}>
-
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: 30 }}>
-                        <div style={arrowContainerStyleLeft}>
-                            <LeftArrow onClick={navigateToPrevPhoto} />
-                        </div>
-                        <div style={transitionStyle}>
-                            {photos.map((photo, index) => (
-                                <img
-                                    key={index}
-                                    src={photo}
-                                    alt={`Photo ${index + 1}`}
-                                    style={{ flex: '0 0 100%' }}
-                                />
-                            ))}
-                        </div>
-                        <div style={arrowContainerStyleRight}>
-                            <RightArrow onClick={navigateToNextPhoto} />
-                        </div>
-                    </div>
-                </div>
-
-                <Workcard />
-                <Worktype />
-            </div>
-
-        );
+    const photoSliderStyle = {
+      transform: `translateX(-${currentPhotoIndex * 100}%)`,
+      transition: 'transform 0.5s ease-in-out',
+      display: 'flex',
+      width: `${photos.length * 100}%`,
+      backgroundColor: 'gray',
     };
 
     return (
-        <main>
-            <div className="photo-carousel-container">
-                <PhotoCarousel />
+      <div className="photo-carousel-container">
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ backgroundColor: 'red', padding: 10, alignItems: 'center', display: 'flex' }}>
+            <LeftArrow onClick={navigateToPrevPhoto} />
+          </div>
+          <div style={{ borderColor: 'blue', borderRadius: 10, borderStyle: 'solid', borderWidth: 2, overflow: 'hidden', width: '90%', height: mainPhoto }}>
+            <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
+              <div className="" style={photoSliderStyle}>
+                {photos.map((photo, index) => (
+                  <img key={index} className="photo" src={photo} alt={`photo-${index + 1}`} />
+                ))}
+              </div>
             </div>
+          </div>
+          <div style={{ backgroundColor: 'red', padding: 10, alignItems: 'center', display: 'flex' }}>
+            <RightArrow onClick={navigateToNextPhoto} />
+          </div>
+        </div>
+        <Workcard />
+        <Worktype />
+      </div>
+    );
+  };
 
-            {/* Additional content */}
+  return (
+    <main>
+      <div className="photo-carousel-container">
+        <PhotoCarousel />
+      </div>
 
-        </main>
-    )
+      {/* Additional content */}
+    </main>
+  );
 }
 
 export default Home;
